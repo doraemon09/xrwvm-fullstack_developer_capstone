947190c4080edb7fd0a2fe3e2f600e379c2fad61
doraemon09 <1687256+doraemon09@users.noreply.github.com>
2024-10-25 14:40:03 -0700

    Update models.py - Linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -29,8 +29,7 @@ class CarMake(models.Model):
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
-class CarModel(models.Model):    
+class CarModel(models.Model):
     # Many-to-One relationship
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
 
@@ -43,7 +42,11 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators = [MaxValueValidator(2023), MinValueValidator(2015)])
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
     # Other fields as needed
 
     def __str__(self):
